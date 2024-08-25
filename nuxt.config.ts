62bd38fbcbe6d9cbe0b3e79dd12109732b9631cd
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2024-08-25 12:04:17 +0530

    chore: install firebase for nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,27 @@
+import firebaseConfig from "./config/firebaseConfig";
+import type { ModuleOptions as VueFireNuxtModuleOptions } from "nuxt-vuefire";
+
+declare module "@nuxt/schema" {
+  interface NuxtConfig {
+    vuefire?: VueFireNuxtModuleOptions;
+  }
+}
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   extends: ["@nuxt/ui-pro"],
-  modules: ["@nuxt/ui", "@nuxtjs/color-mode", "@vue-macros/nuxt"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxtjs/color-mode",
+    "@vue-macros/nuxt",
+    "nuxt-vuefire",
+  ],
+  vuefire: {
+    auth: {
+      enabled: true,
+    },
+    config: firebaseConfig,
+  },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { useCurrentUser } from 'vuefire'
+
+const user = useCurrentUser()
+</script>
+
 <template>
   <div class="pt-36 pb-44 bg-primary-50 dark:bg-envy-900 text-center">
     <h1 class="font-extrabold text-5xl text-primary-700 dark:text-white">
@@ -6,5 +12,6 @@
     <h2 class="mt-3 text-2xl italic text-primary-800 dark:text-envy-300">
       Fastest way to launch your NFT's metadata
     </h2>
+    <pre>{{ user || "No user yet" }}</pre>
   </div>
 </template>
